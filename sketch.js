5efe24d9ed8ed43cfcef627fa7f45c8cc26621ee
bhartishah <53393160+bhartishah@users.noreply.github.com>
2020-01-12 21:19:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(800,400);
+ //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("yellow");  
+  drawSprites();
+  fill("orange")
+  triangle(60,200,95,150,150,200);
+  rect(65,200,80,300);
+  triangle(355,200,325,150,295,200);
+  rect(295,200,60,350);
+  fill("purple")
+  rect(145,100,150,400);
+  fill("blue")
+ rect(200,350,45,400);
+ fill("pink")
+ rect(150,200,45,50);
+ rect(242,200,45,50);
+ fill("blue")
+ triangle(145,100,225,55,295,100)
+
+}
